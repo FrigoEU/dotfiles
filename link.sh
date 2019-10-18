b0eba98e57ef9087cd962fa77e88e9223dad2351
Simon Van Casteren <simonvancasteren@localhost.localdomain>
2019-10-18 18:39:48 +0200

    Yup

--- link.sh
+++ link.sh
@@ -1,3 +1,4 @@
 ln -s $PWD/init.vim ~/.config/nvim/init.vim
 ln -s $PWD/Frugal.colloquyStyle ~/Library/Application\ Support/Colloquy/Styles/Frugal.colloquyStyle
 ln -s $PWD/.Xdefaults ~/.Xdefaults
+ln -s $PWD/.bashrc ~/.bashrc
