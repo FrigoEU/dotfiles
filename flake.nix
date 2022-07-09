6a2e2b41978c5d447641438115d851b1247811e7
Simon Van Casteren <simon.van.casteren@gmail.com>
2022-07-09 13:56:19 +0200

    Added home-manager and x1 config to flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05"; 
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager }:
     let
       overlay = final: prev: (import nixpkgs {
         system = "x86_64-linux";
@@ -17,6 +21,24 @@
             ./hardware-configuration-desktop.nix
             ./configuration-shared.nix
             ./desktop.nix
+          ];
+        };
+        nixosConfigurations.nixos-x1 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; })
+            ./hardware-configuration-desktop.nix
+            ./configuration-shared.nix
+            ./x1.nix
+            (home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.users.jdoe = import ./home.nix;
+
+                  # Optionally, use home-manager.extraSpecialArgs to pass
+                  # arguments to home.nix
+                })
           ];
         };
       };
