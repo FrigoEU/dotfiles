f0d967a07dfca8ebb4267d2895142aa10d376d98
Simon Van Casteren <simon.van.casteren@gmail.com>
2022-07-09 19:54:25 +0200

    Neovim setup

--- home.nix
+++ home.nix
@@ -2,8 +2,14 @@
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "simon";
-  home.homeDirectory = "/home/simon";
+  home = {
+    username = "simon";
+    homeDirectory = "/home/simon";
+    sessionVariables = {
+      EDITOR = "neovim";
+    };
+    stateVersion = "22.05";
+  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -13,19 +19,36 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  home.file.".config/nvim/init.lua".source = ./init.lua;
 
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
 
+    extraPackages = [
+      pkgs.ripgrep
+    ];
+
     plugins = with pkgs.vimPlugins; [
-      packer-nvim
+      plenary-nvim
+      which-key-nvim
+      project-nvim
+      telescope-nvim
+      telescope-fzf-native-nvim
+      telescope-project-nvim
+      tokyonight-nvim
+      toggleterm-nvim
+      surround-nvim
+      nvim-lspconfig
+
+      # telescope-ui-select-nvim
       (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
     ];
+
   };
 }
